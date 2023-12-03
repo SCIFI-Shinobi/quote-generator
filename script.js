b6d6dafddb047c71ef91b9541ccd83c27bf43651
Eyobel Zeleke <eyobelzeleke@gmail.com>
2023-12-03 13:16:38 -0800

    add a loader.

--- script.js
+++ script.js
@@ -3,10 +3,22 @@ const quoteText=document.getElementById('quote');
 const authorText=document.getElementById('author');
 const xBtn=document.getElementById('x-button');
 const newQuoteBtn=document.getElementById('new-quote');
+const loader=document.getElementById('loader');
+
+function loading(){
+  loader.hidden=false;
+  quoteContainer.hidden=true;
+}
+
+function complete(){
+  loader.hidden=true;
+  quoteContainer.hidden=false;
+}
 
 let apiQuotes=[];
 
 function newQuote(){
+    loading();
     const quote=apiQuotes[Math.floor(Math.random() *apiQuotes.length)];
     // if author field is empty replace it with unknown
     authorText.textContent = quote.author.split(',').length==2 ? 
@@ -14,9 +26,11 @@ function newQuote(){
     // changing style of quote based on length of  the quote
     quote.text.length > 120 ? quoteText.classList.add('long-quote'):quoteText.classList.remove('long-quote');
     quoteText.textContent = quote.text;
+    complete();
   }
 
 async function getQuotes(){
+  loading();
   const apiUrl='https://type.fit/api/quotes';
   try{
     const response =await fetch(apiUrl);
