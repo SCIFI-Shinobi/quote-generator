08a4ad6cd512420c43b78a3e24279f4ec556d820
Eyobel Zeleke <eyobelzeleke@gmail.com>
2023-12-03 12:00:45 -0800

    Add functionality to the two buttons.

--- script.js
+++ script.js
@@ -27,5 +27,14 @@ async function getQuotes(){
   }
 }
 
+// tweet quote
+function tweetQuote(){
+  const twitterUrl=`https://twitter.com/intent/tweet?text=${quoteText.textContent} -
+  ${authorText.textContent}`;
+  window.open(twitterUrl, '_blank');
+}
+// event listeners for button click
+newQuoteBtn.addEventListener('click', newQuote);
+xBtn.addEventListener('click', tweetQuote);
 
 getQuotes();
